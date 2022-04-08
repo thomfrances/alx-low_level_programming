f150d26c0d31100b0730fe4fea5a3bdd1cc96b9a
thomfrances <thomfrancesss@gmail.com>
2022-04-08 10:18:20 -0700

    My fourth commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -7,6 +8,7 @@
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	write(2, str1, 59);
 	return (1);
 }
