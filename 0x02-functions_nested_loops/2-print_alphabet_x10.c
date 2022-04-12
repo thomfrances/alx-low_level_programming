ac1fc9e3ad9a2b2c284a7067a4bea2a4fa8a8747
thomfrances <thomfrancesss@gmail.com>
2022-04-12 12:31:50 -0700

    My fifth commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,17 @@ void print_alphabet_x10(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 9; a++)
+	b = 0;
+
+	while (b > 10)
 	{
-		for (b = 97; b <= 122; b++)
+		for (a = 0; a <= 9; a++)
 		{
-			_putchar(b);
+			for (b = 97; b <= 122; b++)
+			{
+				_putchar(b);
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
