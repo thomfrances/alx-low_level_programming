7b48484f558db4a8239676c814ea45eeb34df9eb
thomfrances <thomfrancesss@gmail.com>
2022-04-13 07:52:44 -0700

    My Third Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- *main - function prints text as output
+ * main - Prints Holberton as a message.
  *
- *Return:return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char school[10] = "_putchar";
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
 
-	int i;
-
-	for (i = 0; i < 8; i++)
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-		_putchar(school[i]);
+		_putchar(str[count]);
 	}
-	_putchar(10);
+	_putchar('\n');
 	return (0);
 }
