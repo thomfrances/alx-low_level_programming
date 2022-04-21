6673ab1e6450cfe772a8b4a5de837a5d61eb11dd
thomfrances <thomfrancesss@gmail.com>
2022-04-21 14:17:59 -0700

    My seventh commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,22 +5,22 @@
  * @s: input string.
  * Return: integer.
  */
-int _atoi (char *s)
+int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
 	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
+		break;
 
 		if (*(s + count) == '-')
-			pn *= -1;
+		pn *= -1;
 
 		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
 			if (size > 0)
-				m *= 10;
+			m *= 10;
 			size++;
 		}
 		count++;
